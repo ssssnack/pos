81822ade781b54981d2faa1b4ed2f15bc527d270
Snack <247770638@qq.com>
2017-08-25 17:42:53 +0800

    Initial commit

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="pos">
+        <h1>    hello</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'pos',
+    data() {
+        return {
+        };
+    }
+}
+</script>
+<style lang="css" scoped>
+
+</style>
