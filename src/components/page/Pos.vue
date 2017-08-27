9fecc97738a9cdaf67f51ad0cc2d33b8d8e09789
ssssnack <247770638@qq.com>
2017-08-27 22:22:42 +0800

    27

--- src/components/page/Pos.vue
+++ src/components/page/Pos.vue
@@ -1,11 +1,25 @@
 <template>
     <div class="pos">
-        <h1>    hello</h1>
+        <el-row>
+            <el-col :span="7" class="pos-order" id="order-list">
+                我是订单栏
+            </el-col>
+            <el-col :span="17">
+                我是产品栏
+            </el-col>
+
+        </el-row>
     </div>
 </template>
 <script>
 export default {
     name: 'pos',
+    //查看技术胖的生命周期， mounted的意思就是当所有的虚拟dom加载完成之后它要执行的方法。
+    mounted:function(){
+        var orderHeight=document.body.clientHeight;
+        console.log(orderHeight);
+        document.getElementById('order-list').style.height=orderHeight+'px';
+    },
     data() {
         return {
         };
@@ -13,5 +27,8 @@ export default {
 }
 </script>
 <style lang="css" scoped>
-
+.pos-order{
+    background-color: #f9fafc;
+    border-right: 1px solid #c0ccda;
+}
 </style>
